57db7d919a70441f18ea2118872ac8a65311f33d
AlexJunior <alexmomijunior@gmail.com>
2020-07-03 06:40:01 -0300

    cricao do script de web scrap

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def carregar_rendimentos(caminho_do_arquivo):
 
 # Arquivos utilizados
 rendimentos = carregar_rendimentos('datasets/rendimentos.csv')
-carteira_atual = carregar_carteira_atual('datasets/carteira_cot.csv')
+carteira_atual = carregar_carteira_atual('datasets/carteira_atual.csv')
 
 
 option = st.sidebar.selectbox(label='Opções',
@@ -28,18 +28,22 @@ option = st.sidebar.selectbox(label='Opções',
 
 
 if option == 'Carteira Atual':
-    labels_carteira_atual = {'codigo': 'Ativos',
-                             'valor_total': 'Valor Aplicado',
-                             'valor_atual': 'Saldo Bruto'}
     st.title(body='Carteira Atual')
+    tipo_carteira_atual = st.selectbox(label='Selecione',
+                                       options=['Por ação', 'Por classe de ativo'])
 
-    plot = px.pie(carteira_atual, names='codigo',
-                  values='valor_atual', title='Carteira Atual',
-                  labels=labels_carteira_atual,
-                  color_discrete_sequence=px.colors.cmocean.balance,
-                  hover_data=['valor_total'],
-                  hover_name='codigo')
-    st.plotly_chart(plot)
+    if tipo_carteira_atual == 'Por ação':
+        plot_acoes = px.pie(data_frame=carteira_atual, names='Cód. de Negociação',
+                            values='Valor (R$)',
+                            color_discrete_sequence=px.colors.cmocean.balance,
+                            hover_name='Cód. de Negociação')
+        plot_acoes.update_traces(textfont={"size": 15}, overwrite=True)
+        st.plotly_chart(plot_acoes)
+    elif tipo_carteira_atual == 'Por classe de ativo':
+        plot_classe = px.pie(data_frame=carteira_atual, names='Classe',
+                             values='Valor (R$)',
+                             color_discrete_sequence=px.colors.cmocean.balance)
+        st.plotly_chart(plot_classe)
 
     if st.checkbox('Mostrar tabela'):
         carteira_atual
@@ -47,7 +51,8 @@ if option == 'Carteira Atual':
 elif option == 'Rendimentos Mensais':
     st.title(body='Rendimentos Mensais')
     month = st.sidebar.slider('month', 1, 12, value=(3, 7))
-    selecao = (rendimentos['data_pagamento'].dt.month >= month[0]) & (rendimentos['data_pagamento'].dt.month <= month[1])
+    selecao = ((rendimentos['data_pagamento'].dt.month >= month[0]) &
+              (rendimentos['data_pagamento'].dt.month <= month[1]))
     data_filtrada = rendimentos[selecao]
     label = {'ativo': 'Ativo',
             'data_pagamento': 'Data Pagamento',
@@ -55,9 +60,14 @@ elif option == 'Rendimentos Mensais':
             'tipo': 'Tipo',
             'mes_pagamento': 'Mês do Pagamento'}
 
-    plot = px.bar(data_filtrada, x='mes_pagamento', y='total', color='ativo',
-                labels= label, hover_data=['tipo'], width=1000, hover_name='ativo',
+    plot_rend_mensal = px.bar(data_filtrada, x='mes_pagamento', y='total', color='ativo',
+                labels= label, hover_data=['tipo'], width=800, hover_name='ativo',
                 text='total')
+    st.plotly_chart(plot_rend_mensal)
+
+    plot = px.bar(rendimentos, x='ativo', y='total', color='ativo', width=400, height=400)
+    plot.update_layout(barmode='stack', xaxis={'categoryorder':'total ascending'},
+                       overwrite=True)
     st.plotly_chart(plot)
 
     if st.checkbox('Mostrar tabela'):
